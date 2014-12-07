b0a02b908f0082069f31d33106366c128d231318
justinjustin000 <sendjc@gmail.com>
2014-12-07 17:40:29 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,26 @@
+plot1 <- function(){
+
+  #Our overall goal here is simply to examine how household energy usage 
+  #varies over a 2-day period in February, 2007. Your task is to reconstruct 
+  #the following plots below, all of which were constructed using the base 
+  #plotting system.
+  
+  #PLOT 1 Replication
+  
+  #uncode the bleow and install dplyr packages if necessary
+  #install.packages("dplyr")
+  
+  library(dplyr)
+  
+  cdata = read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+  cdata$DateTime <- paste(cdata$Date, cdata$Time)
+  cdata$DateTime <- as.Date(cdata$DateTime, format = "%d/%m/%Y %H:%M:%S")
+  
+  subsetted <- filter(cdata, DateTime >= as.Date("2007-02-01 00:00:00"), DateTime < as.Date("2007-02-03 00:00:00"))
+  
+  #start plotting data to a PNG file
+  png(file="plot1.png", width=480, height=480)
+  hist(subsetted$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="Red")
+  dev.off()
+
+}
